55236ee9b7c6d07b69a773338e3b336eb5ed100d
akalya007 <akalya.d2022eee@sece.ac.in>
2025-07-09 19:28:45 +0530

    Keep  logged-in user after page refresh

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import './index.css' // This should contain @tailwind directives
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <App />
+  // </StrictMode>
+);
