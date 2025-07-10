97ce50f472372989e5bf37008d2831f7a7d17939
akalya007 <akalya.d2022eee@sece.ac.in>
2025-07-10 21:40:06 +0530

    sign-up Feature

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -54,8 +54,8 @@ const Requests = () => {
         return (
           <div
             key={_id}
-            className=" flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-2/3 mx-auto"
-          >
+            className=" flex justify-between items-center m-4 p-4 rounded-lg bg-base-300  mx-auto"
+            >
             <div>
               <img
                 alt="photo"
