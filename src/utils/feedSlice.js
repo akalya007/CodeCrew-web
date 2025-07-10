01c8446d6b8fcd453ea2d4851cd597e5daf09452
akalya007 <akalya.d2022eee@sece.ac.in>
2025-07-10 20:23:52 +0530

    Send/Ignore the user card from the feed

--- src/utils/feedSlice.js
+++ src/utils/feedSlice.js
@@ -7,11 +7,12 @@ const feedSlice = createSlice({
     addFeed: (state, action) => {
       return action.payload;
     },
-    removeFeed: () => {
-      return null;
+   removeUserFromFeed: (state, action) => {
+      const newFeed = state.filter((user) => user._id !== action.payload);  //action.payload is the id which will be sending.
+      return newFeed;
     },
   },
 });
 
-export const { addFeed } = feedSlice.actions;
+export const { addFeed, removeUserFromFeed } = feedSlice.actions;
 export default feedSlice.reducer;
