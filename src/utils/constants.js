717b4b42833cfbdcbb73a3ac4d9738d69c9c2c1b
akalya_d <129743180+akalya007@users.noreply.github.com>
2026-01-28 21:30:41 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // export const BASE_URL =  "http://localhost:3000";
 // export const BASE_URL =  "/api";
 
-export const BASE_URL =  "https://connecthub-backend-1j87.onrender.com";
+export const BASE_URL =  "https://connecthub-backend-ca0z.onrender.com";
 
 
 // export const BASE_URL =  "http://localhost:3000";
